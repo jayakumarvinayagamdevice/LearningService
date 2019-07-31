d868a41a2b6dec78d9a65dae4393709a05470bf9
Jayakumar <Jayakumar@DESKTOP-HF8154Q>
2019-08-01 00:22:20 +0530

    initial library

---
+++ AboutSoftware.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AboutSoftware.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ AboutSoftware.Persistence/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AboutSoftware.Persistence
+{
+    public class Class1
+    {
+    }
+}
